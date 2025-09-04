513835ed78d8fcff1e542cd8ae4d1b64ae22535c
GabeCodesAtSchool <gabe.tender@newharmonyhigh.org>
2025-09-04 16:24:47 +0000

    made my site full

--- index.js
+++ index.js
@@ -19,3 +19,23 @@ function furstSupply() {
         document.getElementById("supply1").className = "joy";
     }
 }
+function interests() {
+    x = Math.floor(Math.random() * 5);
+    if (x === 0) {
+        document.getElementById("supply").innerHTML = "Video Games";
+        document.getElementById("supply").className = "goals";
+        alert(hi);
+    } else if (x === 1) {
+        document.getElementById("supply").innerHTML = "Coding";
+        document.getElementById("supply").className = "sheet";
+    } else if (x === 2) {
+        document.getElementById("supply").innerHTML = "DnD";
+        document.getElementById("supply").className = "notes";
+    } else if (x === 3) {
+        document.getElementById("supply").innerHTML = "Resting";
+        document.getElementById("supply").className = "write";
+    } else if (x === 4) {
+        document.getElementById("supply").innerHTML = "Writing";
+        document.getElementById("supply").className = "joy";
+    }
+}
