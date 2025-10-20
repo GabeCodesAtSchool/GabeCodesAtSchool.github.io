9cc16b11a52f49f113a4843e6f502676c6f54d3c
GabeCodesAtSchool <gabe.tender@newharmonyhigh.org>
2025-10-20 17:04:50 +0000

    Did Quien Es

--- index.js
+++ index.js
@@ -39,3 +39,93 @@ function interests() {
         document.getElementById("supply").className = "joy";
     }
 }
+// Coder Candy 1: Frozen
+// Done on October 19, 2025
+let the_first_time;
+const forever = {
+  do: "1",
+  you: "2",
+  wanna: "3",
+  build: "4",
+  a: "5",
+  snowman: "6",
+  cmon: 1,
+  lets: 3,
+  go: 4,
+  and: 5,
+  play: 6,
+  i: 7,
+  never: 12,
+  see: 67,
+  u: 62,
+  anymore: true,
+  come: "love",
+  out: "can",
+  the: "thaw",
+  door: "no spoilers",
+  its: 5,
+  like: 273,
+  youve: 27291,
+  gone: 19,
+  awayyyyyyyyyy: 69
+}
+let it_go;
+for (the_first_time in forever) {
+  console.log(the_first_time);
+  /* 
+  for the first time in forever
+  my code can actually rhyme!
+  */
+}
+// Coder Candy 1 done
+function reveal1() {
+    document.getElementById("text1").innerHTML = "Ms. Stephanie";
+}
+function reveal2() {
+    document.getElementById("text2").innerHTML = "Gabriel";
+}
+function reveal3() {
+    document.getElementById("text3").innerHTML = "Taylor Swift";
+}
+function reveal4() {
+    document.getElementById("text4").innerHTML = "Mr. Jaron (da BOSS)";
+}
+function reveal5() {
+    document.getElementById("text5").innerHTML = "Beth G. Miranda";
+}
+// Coder Candy 2: Coding In Public / Frozen (Part 2)
+/* A classmate saw me coding and 
+ he said "Yo, he's gonna bring back YouTube!" 
+ I won't bring back YouTube, but I will make something
+ even more fun!*/
+it_go = 0;
+ let snowman;
+ const summer = {
+    we: 2,
+    used: 2,
+    to: 3,
+    be: 3,
+    best: 3,
+    buddies: 3,
+    and: 333,
+    now: 21,
+    were: 2,
+    not: 2,
+    eye: 22,
+    wish: 222,
+    you: true,
+    would: 22,
+    tell: 22,
+    me: 2222,
+    whyyyyyyyyy: 22
+ }
+ for (snowman in summer) {
+    console.log(snowman);
+    melt();
+ }
+ function melt() {
+    it_go++;
+    it_go = 0;
+ }
+ // Coder Candy 2 done
+
