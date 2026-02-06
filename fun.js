a5eaf03d0b1b9c1867121f3a9125bff50d7d2cda
GabeCodesAtSchool <gabe.tender@newharmonyhigh.org>
2026-02-06 00:56:39 +0000

    coded game

--- fun.js
+++ fun.js
@@ -1,4 +1,4 @@
-let moving = false;
+/* let moving = false;
 let leftPos = 0;
 function animate() {
     document.getElementById("char").scrollIntoView({behavior: 'smooth', block: 'center', inline: "center"});
@@ -10,4 +10,42 @@ function animate() {
 setInterval(animate, 30);
 function move() {
     moving = !moving;
+} */
+const moving = {
+    left: false,
+    right: false,
+    up: false,
+    down: false
 }
+const pos = {
+    left: 0,
+    top: 0,
+}
+var leftPos = 0;
+function css(id, property, amount) { 
+  document.getElementById(id).style[property] = `${amount}px`; 
+  }
+setInterval(animate, 30);
+function animate() {
+    document.getElementById("char").scrollIntoView({behavior: 'smooth', block: 'center', inline: "center"});
+    if (moving.left && !moving.right) {
+        pos.left += 3;
+    }
+    if (moving.right && !moving.left) {
+        pos.left -= 3;
+    }
+    if (moving.up && !moving.down) {
+        pos.top -= 3;
+    }
+    if (moving.down && !moving.up) {
+        pos.top += 3;
+    }
+  css("char", "left", pos.left);
+  css("char", "top", pos.top);
+}
+function move(con) {
+for (let key in moving) {
+        moving[key] = false;
+    }
+    moving[con] = true;
+  }
